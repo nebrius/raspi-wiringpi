9a254e88ba5e68bc55cbfba2b08b198c3f42a402
Bryan Hughes <bryan@theoreticalideations.com>
2014-12-08 12:03:32 -0500

    Beefed up error checking

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+
+## 1.0.3 (2014-12-8)
+
+- Beefed up error checking
+
 ## 1.0.2 (2014-12-3)
 
 - Now skips installing Wiring Pi if it's already installed
--- install-wiringpi.sh
+++ install-wiringpi.sh
@@ -24,18 +24,27 @@
 CLONE_DIR=$(mktemp -d)
 cd $CLONE_DIR
 
+# Check if Wiring Pi is already installed
 if command -v gpio >/dev/null 2>&1
 then
   echo "Wiring Pi is already installed, skipping\n"
-else
-  echo "Installing I2C tools"
-  sudo apt-get install -y libi2c-dev
-
-  echo "\nDownloading Wiring Pi...\n"
-  git clone git://git.drogon.net/wiringPi
-  cd wiringPi
-
-  echo "\nBuilding Wiring Pi. You may be asked for your root password.\n"
-  sudo ./build
-  sudo rm -r $CLONE_DIR
+  exit 1
 fi
+
+# Check if git is installed or not
+if !(command -v git >/dev/null 2>&1)
+then
+  echo "You must install git before installing raspi-wiringpi\n"
+  exit 1
+fi
+
+echo "Installing I2C tools"
+sudo apt-get install -y libi2c-dev || { echo "Could not install I2C libraries\n"; exit 1; }
+
+echo "\nDownloading Wiring Pi...\n"
+git clone git://git.drogon.net/wiringPi || { echo "Could not download Wiring Pi\n"; exit 1; }
+cd wiringPi
+
+echo "\nBuilding Wiring Pi. You may be asked for your root password.\n"
+sudo ./build || { echo "Could not install Wiring Pi\n"; exit 1; }
+sudo rm -r $CLONE_DIR
